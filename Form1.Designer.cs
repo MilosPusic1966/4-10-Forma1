79d67cf996787ab37f8954e9bb894b3060897160
MilosPusic2021 <92999387+MilosPusic2021@users.noreply.github.com>
2021-10-25 12:46:48 +0200

    004

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,10 +37,18 @@ namespace _4_10_Forma1
             this.lbl_ime = new System.Windows.Forms.Label();
             this.lbl_prezime = new System.Windows.Forms.Label();
             this.lbl_ocena = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txt_ID
             // 
+            this.txt_ID.Enabled = false;
             this.txt_ID.Location = new System.Drawing.Point(216, 59);
             this.txt_ID.Name = "txt_ID";
             this.txt_ID.Size = new System.Drawing.Size(82, 20);
@@ -103,11 +111,82 @@ namespace _4_10_Forma1
             this.lbl_ocena.TabIndex = 7;
             this.lbl_ocena.Text = "Ocena";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Dodaj";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(241, 250);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Promeni";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(334, 250);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Brisi";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(426, 291);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = ">>";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(60, 291);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "<<";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_ocena);
             this.Controls.Add(this.lbl_prezime);
             this.Controls.Add(this.lbl_ime);
@@ -134,6 +213,13 @@ namespace _4_10_Forma1
         private System.Windows.Forms.Label lbl_ime;
         private System.Windows.Forms.Label lbl_prezime;
         private System.Windows.Forms.Label lbl_ocena;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,21 +15,34 @@ namespace _4_10_Forma1
     {
         string CS = "Data source=INF_4_PROFESOR\\SQLPBG; Initial catalog=MilosP2021; Integrated security=true";
         DataTable podaci = new DataTable();
+        int red = 0;
         public Form1()
         {
             InitializeComponent();
         }
-
+        private void osvezi()
+        {
+            txt_ID.Text = podaci.Rows[red]["ID"].ToString();
+            txt_ime.Text = podaci.Rows[red]["ime"].ToString();
+            txt_prezime.Text = podaci.Rows[red]["prezime"].ToString();
+            txt_ocena.Text = podaci.Rows[red]["ocena"].ToString();
+            if (red == podaci.Rows.Count - 1)               button1.Enabled = false;
+            button1.Enabled = true;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             SqlConnection veza = new SqlConnection(CS);
             SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Ucenik", veza);
             adapter.Fill(podaci);
             //MessageBox.Show(podaci.Rows.Count.ToString());
-            txt_ID.Text = podaci.Rows[0]["ID"].ToString();
-            txt_ime.Text = podaci.Rows[0]["ime"].ToString();
-            txt_prezime.Text = podaci.Rows[0]["prezime"].ToString();
-            txt_ocena.Text = podaci.Rows[0]["ocena"].ToString();
+            osvezi();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            red++;
+            
+            osvezi();
         }
     }
 }
